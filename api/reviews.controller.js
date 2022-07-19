c38eb25f51022f03569b96a4053f01134c085afe
andresev <andresevaldez@gmail.com>
2022-07-18 23:00:03 -0500

    added apiUpdateReview to update review

--- api/reviews.controller.js
+++ api/reviews.controller.js
@@ -23,6 +23,37 @@ class ReviewsController {
       return res.status(500).json({ error: e.message });
     }
   }
+
+  static async apiUpdateReview(req, res) {
+    try {
+      const reviewId = req.body.review_id;
+      const review = req.body.review;
+
+      const date = new Date();
+
+      const ReviewResponse = await ReviewsDAO.updateReview(
+        reviewId,
+        review,
+        req.body.user_name,
+        date
+      );
+
+      let { error } = ReviewResponse;
+      if (error) {
+        res.status.json({ error });
+      }
+
+      if (ReviewResponse.modifiedCount === 0) {
+        throw new Error(
+          'Unable to update review. User may not be original poster'
+        );
+      }
+
+      res.json({ status: 'success' });
+    } catch (e) {
+      return res.status(500).json({ error: e.message });
+    }
+  }
 }
 
 module.exports = ReviewsController;
